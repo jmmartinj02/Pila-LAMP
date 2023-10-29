362a710824d2c4eef45ffee4e8f8ebcec893c962
jmmartinj02 <146434706+jmmartinj02@users.noreply.github.com>
2023-10-29 20:41:32 +0100

    Add files via upload

---
+++ scriptApache2.sh
@@ -0,0 +1,6 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install -y apache2
+sudo apt install -y git
+sudo apt install -y libapache2-mod-php php-mysql
+sudo apt install -y mariadb-client
---
+++ scriptMariaDB.sh
@@ -0,0 +1,5 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+#instalación de mysql/mariadb
+sudo apt install -y mariadb-server-10.3
+sudo ip route del default
